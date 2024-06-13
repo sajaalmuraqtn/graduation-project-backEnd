48d8d0ac4f970697136562bb64907917c94cbb8a
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-06-14 00:41:55 +0300

    some edit

--- Src/Modules/Advertisement/advertisement.router.js
+++ Src/Modules/Advertisement/advertisement.router.js
@@ -1,3 +1,34 @@
+// import { Router } from "express";
+// import * as AdvertisementController from './Controller/advertisement.controller.js'
+// import ServiceRouter from '../service/service.router.js'
+// import { auth, roles } from "../../Middleware/auth.js";
+//  import fileUpload, { fileValidation } from "../../Services/multer.js";
+// import { asyncHandler } from "../../Services/errorHandling.js";
+// import { validation } from "../../Middleware/validation.js";
+//  import * as validators from './advertisement.validation.js'
+// import { endPoint } from "./advertisment.endpoint.js";
+// const router = Router()
+// router.use('/:advertisementId/services',validation(validators.getSpecificAdvertisement),ServiceRouter);
+// router.get('/getAllCenterProviders', auth(roles.Admin), asyncHandler(AdvertisementController.getAllCenterProviders));
+// router.post('/addCenterProvider', auth(roles.Admin) ,fileUpload(fileValidation.image).single('image'), asyncHandler(AdvertisementController.addCenterProvider));
+// router.put('/updateCenterProvider/:centerProviderId', auth(roles.Center) ,fileUpload(fileValidation.image).single('image'),validation(validators.updateCenterProvider), asyncHandler(AdvertisementController.updateCenterProvider));
+// router.patch('/updateCenterProviderExpiredDate/:centerProviderId', auth(roles.Admin) ,validation(validators.updateExpiredDate), asyncHandler(AdvertisementController.updateCenterProviderExpiredDate));
+// router.get('/getSpecificProvider/:centerProviderId', auth(endPoint.getall),validation(validators.getSpecificCenterProvider), asyncHandler(AdvertisementController.getSpecificCenterProvider));
+// router.get('/getSpecificProviderProfile/profile', auth(roles.Center), asyncHandler(AdvertisementController.getSpecificCenterProfile));
+// router.get('/confirmEmail/:token', asyncHandler(AdvertisementController.confirmEmail));
+// //////////////////////////////////////////
+// router.get('/', auth(endPoint.getall), asyncHandler(AdvertisementController.getAllAdvertisement));
+// router.get('/:advertisementId', validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.getSpecificAdvertisement));
+// router.get('/admin/:advertisementId', auth(endPoint.getall), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.getSpecificAdvertisementAdmin));
+// router.get('/allAdvertisements/active', asyncHandler(AdvertisementController.getActiveAdvertisement));
+// router.get('/getMyAdvertisements',auth(roles.Center), asyncHandler(AdvertisementController.getActiveAdvertisement));
+// router.post('/', auth(roles.Center),fileUpload(fileValidation.image).single('mainImage'), validation(validators.createAdvertisement), asyncHandler(AdvertisementController.createAdvertisement));
+// router.put('/:advertisementId', auth(roles.Center), fileUpload(fileValidation.image).single('mainImage'), validation(validators.updateAdvertisement), asyncHandler(AdvertisementController.updateAdvertisement));
+// router.patch('/restore/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.restoreAdvertisement));
+// router.patch('/softDelete/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.softDeleteAdvertisement));
+// router.delete('/hardDelete/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.hardDeleteAdvertisement));
+// export default router;
+
 import { Router } from "express";
 import * as AdvertisementController from './Controller/advertisement.controller.js'
 import ServiceRouter from '../service/service.router.js'
@@ -6,25 +37,16 @@ import { auth, roles } from "../../Middleware/auth.js";
 import { asyncHandler } from "../../Services/errorHandling.js";
 import { validation } from "../../Middleware/validation.js";
  import * as validators from './advertisement.validation.js'
-import { endPoint } from "./advertisment.endpoint.js";
 const router = Router()
 router.use('/:advertisementId/services',validation(validators.getSpecificAdvertisement),ServiceRouter);
-router.get('/getAllCenterProviders', auth(roles.Admin), asyncHandler(AdvertisementController.getAllCenterProviders));
-router.post('/addCenterProvider', auth(roles.Admin) ,fileUpload(fileValidation.image).single('image'), asyncHandler(AdvertisementController.addCenterProvider));
-router.put('/updateCenterProvider/:centerProviderId', auth(roles.Center) ,fileUpload(fileValidation.image).single('image'),validation(validators.updateCenterProvider), asyncHandler(AdvertisementController.updateCenterProvider));
-router.patch('/updateCenterProviderExpiredDate/:centerProviderId', auth(roles.Admin) ,validation(validators.updateExpiredDate), asyncHandler(AdvertisementController.updateCenterProviderExpiredDate));
-router.get('/getSpecificProvider/:centerProviderId', auth(endPoint.getall),validation(validators.getSpecificCenterProvider), asyncHandler(AdvertisementController.getSpecificCenterProvider));
-router.get('/getSpecificProviderProfile/profile', auth(roles.Center), asyncHandler(AdvertisementController.getSpecificCenterProfile));
-router.get('/confirmEmail/:token', asyncHandler(AdvertisementController.confirmEmail));
-//////////////////////////////////////////
-router.get('/', auth(endPoint.getall), asyncHandler(AdvertisementController.getAllAdvertisement));
+
+router.get('/', auth(roles.Admin), asyncHandler(AdvertisementController.getAllAdvertisement));
 router.get('/:advertisementId', validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.getSpecificAdvertisement));
-router.get('/admin/:advertisementId', auth(endPoint.getall), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.getSpecificAdvertisementAdmin));
+router.get('/admin/:advertisementId', auth(roles.Admin), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.getSpecificAdvertisementAdmin));
 router.get('/allAdvertisements/active', asyncHandler(AdvertisementController.getActiveAdvertisement));
-router.get('/getMyAdvertisements',auth(roles.Center), asyncHandler(AdvertisementController.getActiveAdvertisement));
-router.post('/', auth(roles.Center),fileUpload(fileValidation.image).single('mainImage'), validation(validators.createAdvertisement), asyncHandler(AdvertisementController.createAdvertisement));
-router.put('/:advertisementId', auth(roles.Center), fileUpload(fileValidation.image).single('mainImage'), validation(validators.updateAdvertisement), asyncHandler(AdvertisementController.updateAdvertisement));
-router.patch('/restore/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.restoreAdvertisement));
-router.patch('/softDelete/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.softDeleteAdvertisement));
-router.delete('/hardDelete/:advertisementId', auth(roles.Center), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.hardDeleteAdvertisement));
+router.post('/', auth(roles.Admin),fileUpload(fileValidation.image).single('mainImage'), validation(validators.createAdvertisement), asyncHandler(AdvertisementController.createAdvertisement));
+router.put('/:advertisementId', auth(roles.Admin), fileUpload(fileValidation.image).single('mainImage'), validation(validators.updateAdvertisement), asyncHandler(AdvertisementController.updateAdvertisement));
+router.patch('/restore/:advertisementId', auth(roles.Admin), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.restoreAdvertisement));
+router.patch('/softDelete/:advertisementId', auth(roles.Admin), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.softDeleteAdvertisement));
+router.delete('/hardDelete/:advertisementId', auth(roles.Admin), validation(validators.getSpecificAdvertisement), asyncHandler(AdvertisementController.hardDeleteAdvertisement));
 export default router;
--- Src/Modules/Advertisement/advertisement.validation.js
+++ Src/Modules/Advertisement/advertisement.validation.js
@@ -1,3 +1,78 @@
+// import Joi from "joi"
+// import { generalFieldValidation } from "../../Middleware/validation.js"
+
+// export const createAdvertisement = Joi.object(
+//     {
+//         name: Joi.string().min(3).max(50).required(),
+//         facebookLink: Joi.string(),
+//         instagramLink: Joi.string(),
+//         description: Joi.string().min(50).max(150000).required(),
+//         phoneNumber: Joi.string().min(10).max(10).required(),
+//         file: generalFieldValidation.file.required(),
+//         status: Joi.string().valid('Active', 'Inactive'),
+//         address: Joi.string().min(10).max(100),
+//         city: Joi.string().valid('Hebron','Nablus','Jerusalem','Ramallah','Tulkarm','Jenin','Al-Bireh','Jericho','Yatta','Beit Jala').required()
+//     }
+// )
+
+// export const updateAdvertisement = Joi.object(
+//     {
+//         name: Joi.string().min(3).max(50),
+//         facebookLink: Joi.string(),
+//         instagramLink: Joi.string(),
+//         description: Joi.string().min(50).max(150000),
+//         phoneNumber: Joi.string().min(10).max(10),
+//         file: generalFieldValidation.file,
+//         status: Joi.string().valid('Active', 'Inactive'),
+//         address: Joi.string().min(10).max(100),
+//         city: Joi.string().valid('Hebron','Nablus','Jerusalem','Ramallah','Tulkarm','Jenin','Al-Bireh','Jericho','Yatta','Beit Jala'),
+//         advertisementId: Joi.string().min(24).max(24).required()
+//     }
+// )
+ 
+// export const getSpecificAdvertisement = Joi.object(
+//     {
+//         advertisementId: Joi.string().min(24).max(24).required(),
+//     }
+// )
+
+
+// //////////////////////////////////////////////
+
+// export const addCenterProvider = Joi.object(
+//     {
+//         centerProviderName: Joi.string().min(4).max(30).required(),
+//         email: generalFieldValidation.email,
+//         password: generalFieldValidation.password.required(),
+//         confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+//         phoneNumber: Joi.string().min(10).max(10).required(),
+//         expiredDate: Joi.date().greater('now').required(),
+//         file: generalFieldValidation.file.required(),
+//     }
+// )
+
+// export const getSpecificCenterProvider = Joi.object(
+//     {
+//         centerProviderId: Joi.string().min(24).max(24).required(),
+//     }
+// )
+
+// export const updateCenterProvider = Joi.object(
+//     {
+//         centerProviderName: Joi.string().min(4).max(30).required(),
+//         phoneNumber: Joi.string().min(10).max(10),
+//         file: generalFieldValidation.file,
+//         centerProviderId: Joi.string().min(24).max(24).required()
+//     }
+// )
+
+// export const updateExpiredDate = Joi.object(
+//     {
+//         centerProviderId: Joi.string().min(24).max(24).required(),
+//         expiredDate: Joi.date().greater('now')
+//     }
+// )
+
 import Joi from "joi"
 import { generalFieldValidation } from "../../Middleware/validation.js"
 
@@ -10,6 +85,7 @@ export const createAdvertisement = Joi.object(
         phoneNumber: Joi.string().min(10).max(10).required(),
         file: generalFieldValidation.file.required(),
         status: Joi.string().valid('Active', 'Inactive'),
+        expiredDate: Joi.date().greater('now').required(),
         address: Joi.string().min(10).max(100),
         city: Joi.string().valid('Hebron','Nablus','Jerusalem','Ramallah','Tulkarm','Jenin','Al-Bireh','Jericho','Yatta','Beit Jala').required()
     }
@@ -24,6 +100,7 @@ export const updateAdvertisement = Joi.object(
         phoneNumber: Joi.string().min(10).max(10),
         file: generalFieldValidation.file,
         status: Joi.string().valid('Active', 'Inactive'),
+        expiredDate: Joi.date().greater('now'),
         address: Joi.string().min(10).max(100),
         city: Joi.string().valid('Hebron','Nablus','Jerusalem','Ramallah','Tulkarm','Jenin','Al-Bireh','Jericho','Yatta','Beit Jala'),
         advertisementId: Joi.string().min(24).max(24).required()
@@ -33,43 +110,6 @@ export const updateAdvertisement = Joi.object(
 export const getSpecificAdvertisement = Joi.object(
     {
         advertisementId: Joi.string().min(24).max(24).required(),
-    }
-)
-
-
-//////////////////////////////////////////////
-
-export const addCenterProvider = Joi.object(
-    {
-        centerProviderName: Joi.string().min(4).max(30).required(),
-        email: generalFieldValidation.email,
-        password: generalFieldValidation.password.required(),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
-        phoneNumber: Joi.string().min(10).max(10).required(),
-        expiredDate: Joi.date().greater('now').required(),
-        file: generalFieldValidation.file.required(),
-    }
-)
-
-export const getSpecificCenterProvider = Joi.object(
-    {
-        centerProviderId: Joi.string().min(24).max(24).required(),
-    }
-)
-
-export const updateCenterProvider = Joi.object(
-    {
-        centerProviderName: Joi.string().min(4).max(30).required(),
-        phoneNumber: Joi.string().min(10).max(10),
-        file: generalFieldValidation.file,
-        centerProviderId: Joi.string().min(24).max(24).required()
-    }
-)
-
-export const updateExpiredDate = Joi.object(
-    {
-        centerProviderId: Joi.string().min(24).max(24).required(),
-        expiredDate: Joi.date().greater('now')
     }
 )
 
